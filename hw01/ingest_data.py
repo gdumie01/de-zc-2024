e660ab4c1ce004a14f15875681a0905bf7daa45f
Gonçalo Dumiense <22841055+gdumie01@users.noreply.github.com>
2024-01-29 21:24:17 +0000

    Added docker compose to run the environment

--- hw01/ingest_data.py
+++ hw01/ingest_data.py
@@ -65,13 +65,13 @@ def main(params):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
+    parser.add_argument('--user', required=False, help='user name for postgres', default=os.environ.get('USER'))
+    parser.add_argument('--password', required=False, help='password for postgres', default=os.environ.get('PASSWORD'))
+    parser.add_argument('--host', required=False, help='host for postgres', default=os.environ.get('HOST'))
+    parser.add_argument('--port', required=False, help='port for postgres', default=os.environ.get('PORT'))
+    parser.add_argument('--db', required=False, help='database name for postgres', default=os.environ.get('DB'))
+    parser.add_argument('--table_name', required=False, help='name of the table where we will write the results to', default=os.environ.get('TABLE_NAME'))
+    parser.add_argument('--url', required=False, help='url of the csv file', default=os.environ.get('URL'))
 
     args = parser.parse_args()
 
