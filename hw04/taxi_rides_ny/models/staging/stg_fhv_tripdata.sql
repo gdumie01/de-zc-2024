900ae6c1c942df20f3b603d16592e4fad0c9b54f
Gonçalo Dumiense <goncalo.dumiense@gmail.com>
2024-02-22 12:06:25 +0000

    Removed tests from FHV model

--- hw04/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ hw04/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -19,7 +19,5 @@ where EXTRACT(YEAR from pickup_datetime) = 2019
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
 {% if var('is_test_run', default=true) %}
-
   limit 100
-
 {% endif %}
