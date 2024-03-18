93e7cbc8e29d7563f9bc0bfa109e15415bafd10b
gdumie01 <22841055+gdumie01@users.noreply.github.com>
2024-03-18 22:10:53 +0000

    correct Q3

--- ws02/Q3.sql
+++ ws02/Q3.sql
@@ -3,9 +3,12 @@ WITH latest AS (
     SELECT MAX(tpep_pickup_datetime) AS latest_pickup_time
     FROM trip_data
 )
-SELECT tz1.zone, COUNT(*) 
-FROM trip_data t1, latest
+SELECT tz1.zone, COUNT(*) AS trips_num
+FROM trip_data t1
+LEFT JOIN latest ON 1=1
 LEFT JOIN taxi_zone tz1 ON t1.PULocationID = tz1.location_id
-WHERE tpep_pickup_datetime > DATE_SUB(latest_pickup_time, INTERVAL '17' HOUR)
-GROUP BY 1;
+WHERE tpep_pickup_datetime > (latest_pickup_time - INTERVAL '17 hours')
+GROUP BY tz1.zone;
+
+SELECT * FROM trip_stats_3 ORDER BY trips_num DESC LIMIT 3;
 
